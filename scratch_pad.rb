6627f0289db3f84143c94ca4d0b9428a8fff6b01
Peter Haugen <phaugen@gmail.com>
2014-11-12 20:11:45 -0600

    figured out how to make grid of divs

--- scratch_pad.rb
+++ scratch_pad.rb
@@ -1,3 +1,16 @@
+# interested course of events
+# 1. figure out how to print to files.
+# 2. figure out how to make a checkerboard of divs in css.
+# 3. out put a
+#
+#
+#
+#
+#
+#
+#
+
+
 begin
   file = File.open("./test.css", mode = "w")
   10.times {|i| file.write("#{i}th line, dawg\n")}
